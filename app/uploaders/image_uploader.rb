75aac459c30a25a08660e3ad47ba7ac8372b22bf
everytime1004 <ha621209@naver.com>
2013-05-27 12:17:43 +0900

    ASD

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,8 +6,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -25,9 +25,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    # For Rails 3.1+ asset pipeline compatibility:
-    # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  
     "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
 
